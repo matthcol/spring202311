e736ec1b95a021653180cca54ef554405e42b3fd
matthcol <matthias.colin@gmail.com>
2023-11-21 17:03:51 +0100

    service movie interface

---
+++ src/main/java/org/example/movieapi/service/MovieService.java
@@ -0,0 +1,14 @@
+package org.example.movieapi.service;
+
+import org.example.movieapi.dto.MovieDetail;
+import org.example.movieapi.dto.MovieSimple;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieService {
+
+    List<MovieSimple> findAll();
+
+    Optional<MovieDetail> findById(int id);
+}
