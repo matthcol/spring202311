5ac1057be661a852a70fa72b8ff84b0f87d0958d
matthcol <matthias.colin@gmail.com>
2023-11-22 13:44:10 +0100

    add features in movie service

--- src/main/java/org/example/movieapi/service/MovieService.java
+++ src/main/java/org/example/movieapi/service/MovieService.java
@@ -11,7 +11,34 @@ public interface MovieService {
 
     List<MovieSimple> findAll();
 
+    /**
+     *
+     * @param id
+     * @return Optional empty if not found
+     */
     Optional<MovieDetail> findById(int id);
 
     MovieSimple add(MovieCreate movie);
+
+    /**
+     *
+     * @param movie
+     * @return Optional empty if not found
+     */
+    Optional<MovieSimple> update(MovieSimple movie);
+
+    /**
+     *
+     * @param idMovie
+     * @param idDirector
+     * @return Optional empty if movie or director not found
+     */
+    Optional<MovieDetail> setDirector(int idMovie, int idDirector);
+
+    /**
+     *
+     * @param id
+     * @return false if not found
+     */
+    boolean delete(int id);
 }
